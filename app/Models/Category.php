932a43156fc30a81c54bbe871d7947060fa2a690
Zein7171 <zeiinnagasta@gmail.com>
2026-05-05 07:36:12 +0700

    update

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -3,8 +3,17 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Category extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['name', 'slug'];
+
+    // Menandakan atribut: 1 Kategori dapat memiliki banyak list Event
+    public function events()
+    {
+        return $this->hasMany(Event::class);
+    }
 }
